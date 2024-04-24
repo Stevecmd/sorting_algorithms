20e7a103e9b950f61e12b440ece1682616df18fc
Stevecmd <murimi101@gmail.com>
2024-04-24 12:17:34 +0300

    Initial file setup

---
+++ sort.h
@@ -0,0 +1,59 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+/* Protoypes */
+/* 0. Bubble sort */
+void bubble_sort(int *array, size_t size);
+
+/* 1. Insertion sort */
+void insertion_sort_list(listint_t **list);
+
+/* 2. Selection sort  */
+void selection_sort(int *array, size_t size);
+
+/* 3. Quick sort  */
+void quick_sort(int *array, size_t size);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* SORT_H */
