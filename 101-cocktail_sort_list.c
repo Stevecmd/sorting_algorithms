6d19e6e0db3e978ecf67cc20822ba403e2f48045
Stevecmd <murimi101@gmail.com>
2024-04-30 09:46:35 +0300

    update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,8 +1,5 @@
 #include "sort.h"
 
-void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
-void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
-
 /**
  * cocktail_sort_list - Sorts a doubly linked list of integers
  *                     in ascending order
